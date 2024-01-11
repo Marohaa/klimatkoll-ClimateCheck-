f3e0db44988972437ee9e46dbd75918445af37e7
Ammara-hb <144907014+Ammara-hb@users.noreply.github.com>
2024-01-11 19:02:13 +0100

    Add files via upload

---
+++ src/quiz/quiz.jsx
@@ -0,0 +1,154 @@
+import React, { useEffect, useState } from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
+import Question from "./Question";
+import QuizResult from "./QuizResult";
+import shuffleArray from "./utils";
+
+const Quiz = () => {
+  const [QuizData, setQuizData] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [modalShow, setModalShow] = React.useState(false);
+
+  useEffect(() => {
+    const url = "/data/quizdata.json";
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        data.map(
+          (p) => (p.all_answers = [p.correct_answer, ...p.incorrect_answers])
+        );
+        shuffleArray(data);
+        data.map((p) => shuffleArray(p.all_answers));
+        setQuizData(data);
+      });
+  }, []);
+
+  const handlePrev = () => {
+    if (currentIndex > 0) setCurrentIndex(currentIndex - 1);
+  };
+  const handleNext = () => {
+    if (currentIndex < QuizData.length - 1) setCurrentIndex(currentIndex + 1);
+  };
+
+  const onAnswer = (ans) => {
+    QuizData[currentIndex].isCorrect =
+      QuizData[currentIndex].correct_answer === ans;
+    QuizData[currentIndex].currentUserAnswer = ans;
+    setQuizData([...QuizData]);
+  };
+
+  const handleFinish = () => {
+    setModalShow(true);
+  };
+
+  const handleReset = () => {
+    setCurrentIndex(0);
+    QuizData.map((p) => (p.currentUserAnswer = ""));
+    shuffleArray(QuizData);
+    QuizData.map((p) => shuffleArray(p.all_answers));
+    setQuizData([...QuizData]);
+  };
+
+  return (
+    <div style={{marginBottom: ''}}>
+      <Container>
+        <Row>
+          <Col md={6}>
+            {/*here we use background image in an div.*/}
+            <div>
+              {" "}
+              <img
+                className=""
+                src="bilder/10009641.png"
+                style={{ height: "700px", width: "700px" }}
+              ></img>{" "}
+            </div>
+          </Col>
+
+          <Col md={6}>
+            <div className="mt-5" 
+            style={{
+              
+            }}>
+
+            <Question
+  data={QuizData[currentIndex]}
+  index={currentIndex}
+  numberOfQuestion={QuizData.length}
+  onAnswer={onAnswer}
+></Question>
+
+<Container className="my-2">
+  <Row>
+    <Col xs={12} style={{ textAlign: "center" }}>
+
+      {currentIndex !== QuizData.length - 10 && (
+        <a className="btn rounded-pill text-white"
+          
+          style={{
+            float: "left",
+            backgroundColor: "#4CAF83",
+            marginTop: "3rem",
+            minWidth: '150px'
+          }}
+          onClick={handlePrev}
+        >
+          Tillbaka
+        </a>
+      )}
+
+      {currentIndex !== QuizData.length - 1 && (
+        <a className="btn rounded-pill text-white"
+          
+          style={{
+            float: "right",
+            backgroundColor: "#4CAF83",
+            marginTop: "3rem",
+            minWidth: '150px',
+          }}
+          onClick={handleNext}
+        >
+          Nästa
+        </a>
+      )}
+
+      {currentIndex == QuizData.length - 1 && (
+        <a className="btn rounded-pill text-white"
+          
+          style={{
+            float: "right",
+            backgroundColor: "#4CAF83",
+            marginTop: "3rem",
+            minWidth: '150px',
+          }}
+          onClick={handleFinish}
+        >
+          Slutför
+        </a>
+
+      )}
+    </Col>
+  </Row>
+</Container>
+
+
+
+          </div>
+          </Col>
+        </Row>
+      </Container>
+
+      <QuizResult
+        show={modalShow}
+        onHide={() => {
+          setModalShow(false);
+          handleReset();
+        }}
+        numberOfQuestion={QuizData.length}
+        numberOfCorrectAnswers={QuizData.filter((p) => p.isCorrect).length}
+      ></QuizResult>
+    </div>
+  );
+};
+
+export default Quiz;
